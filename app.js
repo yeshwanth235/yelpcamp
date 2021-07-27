16bbf312dc36fe3a37930b027f1d523175600c2d
yeshwanth235 <m.yeshwanth235@gmail.com>
2021-07-27 10:10:15 +0530

    fix error6

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dbc7chzdc/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
