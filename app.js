0d3e1f0d51d6b5167c359b72daabb9f79c64f274
yeshwanth235 <m.yeshwanth235@gmail.com>
2021-07-27 07:37:04 +0530

    fixed errors4

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(mongoSanitize({
 }))
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-const store = new MongoDBStore({
+const store = new MongoStore({
     url: dbUrl,
     secret,
     touchAfter: 24 * 60 * 60
